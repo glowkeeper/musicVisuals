022d2018a421a46bc3876b7cf74c032a1eadab6c
glowkeeper <steve@glowkeeper.uk>
2020-06-29 13:00:58 +0100

    Tweaks

--- src/components/pages/main.tsx
+++ src/components/pages/main.tsx
@@ -1,44 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import * as d3 from "d3"
+//import * as d3 from "d3"
 
-import { guess } from 'web-audio-beat-detector'
-//import { analyze } from 'web-audio-beat-detector'
+//import { guess } from 'web-audio-beat-detector'
+import { analyze } from 'web-audio-beat-detector'
 import { AudioContext } from 'standardized-audio-context'
 
 import { App } from '../../config/strings'
 
 import { themeStyles } from '../../styles'
-
-function getData(audioCtx: any): any {
-
-    let source = audioCtx.createBufferSource();
-    var request = new XMLHttpRequest();
-
-    request.open('GET', "./collegeCampus.wav", true)
-    request.responseType = 'arraybuffer'
-
-    request.onload = function() {
-      var audioData = request.response;
-
-      audioCtx.decodeAudioData(audioData, function(buffer: any) {
-          source.buffer = buffer
-          source.connect(audioCtx.destination)
-          guess(source.buffer)
-            .then(({ bpm, offset }: any) => {
-                console.log("track info: ", bpm, offset)
-            })
-            .catch((err: any) => {
-                console.log(err)
-            });
-          //source.loop = true
-        },
-        function(e: any){ console.log("Error with decoding audio data" + e.err); })
-
-    }
-
-    request.send()
-    return source
-}
 
 export const Main = () => {
 
@@ -47,10 +16,14 @@ export const Main = () => {
     const themeClasses = themeStyles()
 
     const classes = themeStyles()
-    const audioCtx = new AudioContext()
+    const ctx = new AudioContext()
+
+    let analyser = ctx.createAnalyser()
+    analyser.fftSize = 256
+    const bufferLength = analyser.frequencyBinCount
+    let dataArray = new Uint8Array(bufferLength)
 
     let source: any
-    let audioTempo = 0
 
     var width = Math.max(960, innerWidth),
         height = Math.max(500, innerHeight);
@@ -63,7 +36,60 @@ export const Main = () => {
         r = 200,
         τ = 2 * Math.PI;
 
-    const drawCanvas = (ref: any) => {
+    let canvasCtx: any
+    let drawVisual: any
+
+    const drawInit = (ref: any) => {
+        //console.log('this is the canvas DOM element you want', ref)
+        if ( ref !== null ) {
+
+            canvasCtx = ref.getContext("2d")
+            drawVisual = requestAnimationFrame(draw);
+
+            analyser.getByteTimeDomainData(dataArray)
+
+            canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+            canvasCtx.fillRect(0, 0, width, height);
+
+            canvasCtx.lineWidth = 2;
+            canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+            canvasCtx.beginPath();
+
+        }
+    }
+
+    const draw = () => {
+
+      //console.log("here!")
+
+
+      var sliceWidth = width * 1.0 / bufferLength;
+      var x = 0;
+
+      console.log(bufferLength)
+
+      for(var i = 0; i < bufferLength; i++) {
+
+          console.log(dataArray[i] )
+
+        var v = dataArray[i] / 128.0;
+        var y = v * height/2;
+
+        if(i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(width, height/2);
+      canvasCtx.stroke();
+    }
+
+    /*const draw = (ref: any) => {
         //console.log('this is the canvas DOM element you want', ref)
         var context = ref.getContext("2d")
         //context!.globalCompositeOperation = "lighter"
@@ -89,12 +115,13 @@ export const Main = () => {
                 }
               })
         })
-      }
+    }*/
 
       const getAudioData = (): any => {
 
-          source = audioCtx.createBufferSource();
-          var request = new XMLHttpRequest();
+          source = ctx.createBufferSource();
+
+          var request = new XMLHttpRequest()
 
           request.open('GET', "./collegeCampus.wav", true)
           request.responseType = 'arraybuffer'
@@ -103,18 +130,19 @@ export const Main = () => {
             var audioData = request.response;
             //console.log(audioData)
 
-            audioCtx.decodeAudioData(audioData, function(buffer: any) {
+            ctx.decodeAudioData(audioData, function(buffer: any) {
                 source.buffer = buffer as AudioBuffer
-                source.connect(audioCtx.destination)
+                source.connect(ctx.destination)
+                analyser.connect(ctx.destination)
 
-                guess(source.buffer)
-                  .then(({ bpm, offset }: any) => {
-                      console.log("track info: ", bpm, offset)
+                analyze(source.buffer)
+                  .then((bpm: any) => {
+                      console.log("BPM: ", bpm)
+                      setTempo(bpm)
                   })
                   .catch((err: any) => {
                       console.log(err)
-                  });
-                //source.loop = true
+                  })
               },
               function(e: any){ console.log("Error with decoding audio data" + e.err); })
 
@@ -125,7 +153,8 @@ export const Main = () => {
 
     const play = () => {
       getAudioData();
-      source.start(0);
+      source.start(0)
+      draw()
     }
 
     const stop = () => {
@@ -136,7 +165,8 @@ export const Main = () => {
 
     return (
       <>
-        <canvas ref={(e) => drawCanvas(e)} width={width} height={height}></canvas>
+        <canvas ref={(e) => drawInit(e)} width={width} height={height}></canvas>
+        <p> Tempo is {tempo}</p>
         <button onClick={() => play()}>play!</button>
         <button onClick={() => stop()}>stop!</button>
 
